5ff68e48485c4d73cf5d03a1974f24499ca6fd45
tsubasaNicolas <nicolas.carrasco.m@gmail.com>
2022-12-14 11:51:42 -0300

    Number(process.env.DB_PORT)

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ export const DB_HOST =
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "4vkPnT7FJa6Mpd4nLeeJ";
 export const DB_DATABASE = process.env.DB_DATABASE || "railway";
-export const DB_PORT = process.env.DB_PORT || 7555;
+export const DB_PORT = Number(process.env.DB_PORT) || 7555;
 
 /* export const PORT = process.env.PORT || 3000;
 export const DB_HOST = process.env.DB_HOST || "localhost";
--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,11 @@
 import { createPool } from "mysql2/promise";
-import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER } from "./config";
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER, DB_PORT } from "./config";
 
 export const pool = createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
-  port: 7555,
-  //port: DB_PORT,
+  // port: 7555,
+  port: DB_PORT,
   database: DB_DATABASE,
 });
