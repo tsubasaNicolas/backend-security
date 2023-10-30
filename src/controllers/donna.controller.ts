45111cb8887b86f04a60396cd5a37a5429a8594d
Nico <nicolas.carrasco.m@gmail.com>
2023-10-29 22:36:25 -0300

    Donna amor 4

--- src/controllers/donna.controller.ts
+++ src/controllers/donna.controller.ts
@@ -2,5 +2,5 @@ import { RequestHandler } from "express";
 
 
 export const getDonna: RequestHandler = async (req, res) => {
-  return res.status(200).send("Te amo amor de mi vida");
+  return res.status(200).send("Te amo");
 };
